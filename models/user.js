ad695f5cce0e7962ec60446033593205caeb7a8e
ijritchey <ijritchey@hotmail.com>
2022-09-10 14:52:54 -0700

    chore: add bcrypt and functions to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 
 const {
   Model
@@ -18,10 +19,10 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       validate: {
-       len: {
-        args: [1,99],
-        msg: 'Name must be between 1 and 99 characters'
-       }
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
       }
     },
     email: {
@@ -36,7 +37,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       validate: {
         len: {
-          args: [8,99],
+          args: [8, 99],
           msg: 'Password must be between 8 and 99 characters'
         }
       }
@@ -45,6 +46,27 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
+  // Before a user is created, we are encrypting the password and using hash in its place
+  user.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
+    // Bcrypt is going to hash the password
+    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
+    pendingUser.password = hash; // this will go to the DB
+  });
+
+  // Check the password on Sign-In and compare it to the hashed password in the DB
+  user.prototype.validPassword = function (typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+
+    return isCorrectPassword;
+  };
+
+  // return an object from the database of the user without the encrypted password
+  user.prototype.toJSON = function () {
+    let userData = this.get();
+    delete userData.password; // it doesn't delete password from database, only removes it. 
+
+    return userData;
+  }
 
   return user; // add functions above 
 };
